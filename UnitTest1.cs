ac6ad7aac6c46ef7f2268bbba587efb003132f4e
warpech <warpech@gmail.com>
2016-03-08 18:35:34 +0100

    initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+//using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTests {
+    [TestFixture("firefox")]
+    //[TestFixture("chrome")]
+    [TestFixture("internet explorer")]
+    public class MarcinNunit {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private bool acceptNextAlert = true;
+        private string browser;
+
+        public MarcinNunit(string browser) {
+            this.browser = browser;
+        }
+
+        [SetUp]
+        public void SetupTest() {
+            switch (browser) {
+                /*case "chrome":
+                    driver = new ChromeDriver();
+                    break;*/
+                case "internet explorer":
+                    InternetExplorerOptions options = new InternetExplorerOptions();
+                    options.IgnoreZoomLevel = true;
+                    //options.IntroduceInstabilityByIgnoringProtectedModeSettings = true;
+                    //options.EnsureCleanSession = true;
+                    driver = new InternetExplorerDriver(options);
+                    break;
+                default:
+                    driver = new FirefoxDriver();
+                    break;
+            }
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest() {
+            try {
+                driver.Quit();
+            }
+            catch (Exception) {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void PageLoads() {
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+            var html = driver.PageSource;
+            StringAssert.Contains("<body", html);
+        }
+
+        [Test]
+        public void BodyElementExists() {
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(driver => driver.FindElement(By.XPath("(//body)[1]")));
+            Assert.IsTrue(IsElementPresent(By.XPath("(//body)[1]")));
+        }
+
+        [Test]
+        public void Polyfill_PageLoads() {
+            driver.Navigate().GoToUrl("https://www.polymer-project.org/1.0/");
+            var html = driver.PageSource;
+            StringAssert.Contains("<body", html);
+        }
+
+        [Test]
+        public void Polyfill_BodyElementExists() {
+            driver.Navigate().GoToUrl("https://www.polymer-project.org/1.0/");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement element = wait.Until(driver => driver.FindElement(By.XPath("(//body)[1]")));
+            Assert.IsTrue(IsElementPresent(By.XPath("(//body)[1]")));
+        }
+
+        private bool IsElementPresent(By by) {
+            try {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException) {
+                return false;
+            }
+        }
+    }
+}
